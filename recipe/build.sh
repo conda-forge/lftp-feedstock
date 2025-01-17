e9efc8a96bebf68b03f48b78c783a42cc4a72988
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-16 16:10:47 -0800

    try to fix cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,24 +15,23 @@ configure_args=(
     --without-libresolv
 )
 
-if [[ ${target_platform} == "osx-arm64" ]]; then
-    export ac_no_dev_ptmx=1
-    export ac_no_dev_ptc=1
-    export CFLAGS="${CFLAGS} -DHAVE_DEV_PTMX=1"
-fi
 
 autoreconf --force --verbose --install
+if [[ ${target_platform} == "osx-arm64" ]]; then
+    sed -i 's/^\*\-\*\-linux\*/*-*-darwin*/' configure
+    export CFLAGS="${CFLAGS} -DHAVE_DEV_PTMX=1"
+fi
 ./configure "${configure_args[@]}"
 make -j$CPU_COUNT
 make install
 make check
 
 # Remove documentation
-rm -rf $uprefix/share/man $uprefix/share/doc
+rm -rf ${PREFIX}/share/man ${PREFIX}/share/doc
 
 # Non-Windows: prefer dynamic libraries to static, and dump libtool helper files
 if [ -z "$VS_MAJOR" ] ; then
     for lib_ident in Xp; do
-        rm -f $uprefix/lib/lib${lib_ident}.la $uprefix/lib/lib${lib_ident}.a
+        rm -f ${PREFIX}/lib/lib${lib_ident}.la ${PREFIX}/lib/lib${lib_ident}.a
     done
 fi
